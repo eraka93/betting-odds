91f557733216bbecdea1503370a2ca65c99cfcb8
eraka93 <eraka.milan@gmail.com>
2023-04-11 22:50:14 +0200

    first

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
+import { useState, useEffect } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [odds, setOdds] = useState([]);
+  const [displayedOdds, setDisplayedOdds] = useState([])
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    getOdds();
+  }, [])
+
+  const getOdds = async () => {
+    try {
+      const response = await fetch('https://www.merkurxtip.rs/restapi/offer/sr/sport/S/mob?annex=0&hours=72&desktopVersion=1.23.4&locale=sr');
+      const data = await response.json()
+      setOdds(data.esMatches);
+      setDisplayedOdds(data.esMatches.slice(0, page * 30))
+      console.log(data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    setDisplayedOdds(odds.slice(0, page * 30))
+  }, [page])
+
+
+  const displayOdds = () => {
+    setPage(prevPage => prevPage + 1)
+  }
+
+  const renderItem = ({ item }) => {
+    return (<Text style={{ color: 'white' }}>{item.home}</Text>)
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={displayedOdds}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        onEndReached={displayOdds}
+        onEndReachedThreshold={0.1}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +57,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#04133D',
     alignItems: 'center',
     justifyContent: 'center',
   },
